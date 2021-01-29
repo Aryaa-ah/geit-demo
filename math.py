4180dc814f218ec5a88b043eb82edc3cd02cf2ee
Aryaa-ah <55133235+Aryaa-ah@users.noreply.github.com>
2021-01-29 15:42:33 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    x = x+y
+    return x+y
+
+
+def subtract(x,y):
+    if x<y:
+        return error
+    return x-y
+def multiply(x,y):
+    pass
+    
+
+def divide(x,y):
+    pass
